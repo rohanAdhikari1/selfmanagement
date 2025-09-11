1ba745a514ee038582f786f312e3674f6b218338
Rohan Adhikari <adhikarirohan249@gmail.com>
2025-09-11 13:07:47 +0545

    feat: Refactor InspectionQuestionInfolist and UserEnrollmentInfolist to use sections for better organization and clarity

--- app/Filament/Resources/UserEnrollments/Schemas/UserEnrollmentInfolist.php
+++ app/Filament/Resources/UserEnrollments/Schemas/UserEnrollmentInfolist.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\UserEnrollments\Schemas;
 
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 
 class UserEnrollmentInfolist
 {
@@ -11,18 +13,48 @@ class UserEnrollmentInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('cleaner.full_name'),
-                TextEntry::make('site.name'),
-                TextEntry::make('from_time')
-                    ->label('Expected From Time')
-                    ->time(),
-                TextEntry::make('to_time')
-                    ->label('Expected To Time')
-                    ->time(),
-                TextEntry::make('created_at')
-                    ->dateTime(),
-                TextEntry::make('updated_at')
-                    ->dateTime(),
+                Section::make('Cleaner Details')
+                    ->icon(Heroicon::User)
+                    ->schema([
+                        TextEntry::make('cleaner.full_name')
+                            ->label('Full Name')
+                            ->placeholder('N/A'),
+                    ]),
+
+                Section::make('Site Information')
+                    ->icon(Heroicon::BuildingOffice)
+                    ->schema([
+                        TextEntry::make('site.name')
+                            ->label('Name')
+                            ->placeholder('Not Assigned'),
+                    ]),
+                Section::make('Schedule')
+                    ->icon(Heroicon::Clock)
+                    ->columns(2)
+                    ->schema([
+                        TextEntry::make('from_time')
+                            ->label('From')
+                            ->time()
+                            ->badge()
+                            ->color('success'),
+                        TextEntry::make('to_time')
+                            ->label('To')
+                            ->time()
+                            ->badge()
+                            ->color('danger'),
+                    ]),
+
+                Section::make('System Info')
+                    ->icon(Heroicon::CalendarDays)
+                    ->columns(2)
+                    ->schema([
+                        TextEntry::make('created_at')
+                            ->label('Created')
+                            ->dateTime('M d, Y H:i'),
+                        TextEntry::make('updated_at')
+                            ->label('Last Updated')
+                            ->dateTime('M d, Y H:i'),
+                    ]),
             ]);
     }
 }
