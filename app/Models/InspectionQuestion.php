abf270a026cd13065b824528368357f081ec84f3
Rohan Adhikari <adhikarirohan249@gmail.com>
2025-09-11 12:43:36 +0545

    feat: Add task relationship to InspectionQuestion model

--- app/Models/InspectionQuestion.php
+++ app/Models/InspectionQuestion.php
@@ -18,4 +18,9 @@ class InspectionQuestion extends Model
         'created_by',
         'updated_by',
     ];
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class, 'task_id');
+    }
 }
