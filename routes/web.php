a61714048cce9addfca7abf9329739a9d3b71682
Rohan Adhikari <adhikarirohan249@gmail.com>
2025-12-16 13:42:30 +0545

    recorn in view

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Livewire\Inspection;
 use App\Livewire\InspectionSurvey;
 use App\Models\CleanerTaskReport;
+use App\Models\Inspectionreport;
 use App\Models\Site;
 use Illuminate\Support\Facades\Route;
 
@@ -11,7 +12,8 @@ Route::middleware('auth:sanctum', 'verified')->group(function () {
     Route::get('/inspection/{report}', InspectionSurvey::class)->name('inspection.survey');
 });
 Route::get('/check', function () {
-    return view('inspect-report-template');
+    $record = Inspectionreport::first();
+    return view('inspect-report-template', ['record' => $record]);
 });
 Route::get('/task', function () {
     $record = CleanerTaskReport::where('report_number', 'REP-0001')->first();
